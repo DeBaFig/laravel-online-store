0eb844303aeed8999439b79bdcf3dd2d67877201
Denize Bassi Figueiredo <denize.f.bassi@gmail.com>
2022-09-08 15:52:19 -0300

    teste

--- online-store-denize/resources/views/layouts/app.blade.php
+++ online-store-denize/resources/views/layouts/app.blade.php
@@ -8,11 +8,11 @@
     <meta name="author" content="" />
     <title>@yield('title', $viewData["title"])</title>
     <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="{{ secure_asset('assets/home/assets/favicon.ico') }}" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/home/assets/favicon.ico') }}" />
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="{{ asset('assets/home/css/styles.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/assets/home/css/styles.css') }}" rel="stylesheet" />
 </head>
 
 <body>
@@ -45,7 +45,8 @@
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
-    <script src="assets/home/js/scripts.js"></script>
+    
+    <script src="{{ asset('assets/home/js/scripts.js') }}"></script>
 </body>
 
-</html>
+</html>  
