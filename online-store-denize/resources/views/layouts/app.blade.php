5ede4df6b33fd2e093a62ebd3cdb06f443c40b5f
Denize Bassi Figueiredo <denize.f.bassi@gmail.com>
2022-09-09 20:14:31 -0300

    deploy heroku - correção https*

--- online-store-denize/resources/views/layouts/app.blade.php
+++ online-store-denize/resources/views/layouts/app.blade.php
@@ -5,14 +5,14 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
-    <meta name="author" content="" />
+    <meta name="author" content="Denize Bassi Figueiredo" />
     <title>@yield('title', $viewData["title"])</title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/home/assets/favicon.ico') }}" />
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="{{ asset('assets/home/css/styles.css') }}" rel="stylesheet" />
+    <link href="assets/home/css/styles.css" rel="stylesheet" />
 </head>
 
 <body>
@@ -46,7 +46,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
     
-    <script src="{{ asset('assets/home/js/scripts.js') }}"></script>
+    <script src="assets/home/js/scripts.js"></script>
 </body>
 
 </html>  
